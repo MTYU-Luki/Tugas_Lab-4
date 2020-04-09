9d71a695528ea2532be4956891a811e3d6a2c055
Luki fiagnia Saputra <luki.saputra@students.amikom.ac.id>
2020-04-09 23:49:37 +0700

    Tugas Lab 4

---
+++ Tugas_Lab-4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("mike", 28);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Thomas", 35, "190302", "Math");
+            teacher.GetNameAndAge();
+
+            Students students = new Students("Sara", 19, "10506", "sara123@amikom.ac.id");
+            students.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
